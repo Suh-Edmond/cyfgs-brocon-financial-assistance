4cc6abc1700558c02310d7005798e680c722bd30
Suh-Edmond <suhedmond25@yahoo.com>
2022-12-16 16:16:43 +0100

    add filter payment items endpoint

--- routes/api.php
+++ routes/api.php
@@ -105,6 +105,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/payment-categories/{payment_category_id}/payment-items', [PaymentItemController::class, 'getPaymentItemsByCategory']);
         Route::get('/payment-categories/{payment_category_id}/payment-items/{id}', [PaymentItemController::class, 'getPaymentItem']);
         Route::get('download-payment-items', [PaymentItemController::class, 'downloadPaymentItem']);
+        Route::get('filter-payment-items', [PaymentItemController::class, 'filterPaymentItems']);
     });
 
 
