90374c7f22a03f4dee6e2eb3328f4ab8d3dc67e2
Suh-Edmond <suhedmond25@yahoo.com>
2023-05-05 13:46:03 +0100

    updated fetch order for sessions

--- database/migrations/2023_04_06_124806_update_payment_item.php
+++ database/migrations/2023_04_06_124806_update_payment_item.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Constants\PaymentItemFrequency;
+use App\Constants\PaymentItemType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,8 +16,8 @@ class UpdatePaymentItem extends Migration
     public function up()
     {
         Schema::table('payment_items', function (Blueprint $table){
-           $table->enum('type', ['ALL_MEMBERS', 'MEMBER_WITH_ROLES', 'MEMBERS_WITHOUT_ROLES', 'A_MEMBER', 'GROUPED_MEMBERS']);
-           $table->enum('frequency', ['YEARLY', 'ONE_TIME', 'MONTHLY', 'QUARTERLY']);
+           $table->enum('type', [PaymentItemType::ALL_MEMBERS, PaymentItemType::A_MEMBER,PaymentItemType::GROUPED_MEMBERS, PaymentItemType::MEMBERS_WITH_ROLES, PaymentItemType::MEMBERS_WITHOUT_ROLES]);
+           $table->enum('frequency', [PaymentItemFrequency::YEARLY, PaymentItemFrequency::QUARTERLY, PaymentItemFrequency::ONE_TIME, PaymentItemFrequency::MONTHLY]);
            $table->longText('reference')->nullable(true);//a particular member or a list of members
         });
     }
