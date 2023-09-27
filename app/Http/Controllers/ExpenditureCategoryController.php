9c875db8d39bd3b872c09a120c660d76adb049f6
Suh-Edmond <suhedmond25@yahoo.com>
2023-09-27 15:40:13 +0100

    add break page to expenditure item details

--- app/Http/Controllers/ExpenditureCategoryController.php
+++ app/Http/Controllers/ExpenditureCategoryController.php
@@ -101,6 +101,7 @@ class ExpenditureCategoryController extends Controller
         $domPdf = $pdf->getDomPDF();
         $canvas = $domPdf->getCanvas();
         $canvas->page_text(10, $canvas->get_height() - 20, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, [0, 0, 0]);
+
         return $pdf->download('Expenditure_Categories.pdf');
     }
 }
