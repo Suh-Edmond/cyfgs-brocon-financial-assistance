ad56f839d6b96b0629ce46412e149d46b40fa68e
Suh-Edmond <suhedmond25@yahoo.com>
2025-05-24 16:21:04 +0100

    update user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Organisation;
 use App\Models\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -28,40 +30,40 @@ class UserSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i = 0; $i < 90; $i++){
-            $created =  User::create([
-                'name'            => $faker->name,
-                'email'           => $faker->email,
-                'telephone'       => $faker->phoneNumber,
-                'gender'          => $faker->randomElement(['MALE', 'FEMALE']),
-                'address'         => $faker->address,
-                'occupation'      => $faker->randomElement(['Teacher', 'Accountant', 'Software Engineer', 'Banker', 'Devops Engineer', 'Nurse', 'Electrician', 'Plumber']),
-                'organisation_id' => $this->organisation[0],
-                'updated_by'      => "James Mark",
-                'status'          => SessionStatus::ACTIVE
-            ]);
-
-            $role = CustomRole::findByName(Roles::MEMBER, 'api');
-            $this->saveUserRole($created, $role,  "James Mark");
-        }
-//        $created =  User::create([
-//            'name'            => "Suh Edmond Neba",
-//            'email'           => "suhedmond25@yahoo.com",
-//            'telephone'       => "+237673660071",
-//            'gender'          => "MALE",
-//            'address'         => "Buea, Cameroon",
-//            'occupation'      => 'Software Engineer',
-//            'organisation_id' => $this->organisation[0],
-//            'updated_by'      => "Edmond",
-//            'status'          => SessionStatus::ACTIVE,
-//            'password'        => Hash::make("Summer123!"),
-//            "email_verified_at" => Carbon::now()
-//        ]);
+//        for($i = 0; $i < 90; $i++){
+//            $created =  User::create([
+//                'name'            => $faker->name,
+//                'email'           => $faker->email,
+//                'telephone'       => $faker->phoneNumber,
+//                'gender'          => $faker->randomElement(['MALE', 'FEMALE']),
+//                'address'         => $faker->address,
+//                'occupation'      => $faker->randomElement(['Teacher', 'Accountant', 'Software Engineer', 'Banker', 'Devops Engineer', 'Nurse', 'Electrician', 'Plumber']),
+//                'organisation_id' => $this->organisation[0],
+//                'updated_by'      => "James Mark",
+//                'status'          => SessionStatus::ACTIVE
+//            ]);
 //
-//        $role = CustomRole::findByName(Roles::MEMBER, 'api');
-//        $role2 = CustomRole::findByName(Roles::ADMIN, 'api');
-//        $this->saveUserRole($created, $role,  "Edmond");
-//        $this->saveUserRole($created, $role2,  "Edmond");
+//            $role = CustomRole::findByName(Roles::MEMBER, 'api');
+//            $this->saveUserRole($created, $role,  "James Mark");
+//        }
+        $created =  User::create([
+            'name'            => "Suh Edmond Neba",
+            'email'           => "suhedmond25@yahoo.com",
+            'telephone'       => "+237673660071",
+            'gender'          => "MALE",
+            'address'         => "Buea, Cameroon",
+            'occupation'      => 'Software Engineer',
+            'organisation_id' => $this->organisation[0],
+            'updated_by'      => "Edmond",
+            'status'          => SessionStatus::ACTIVE,
+            'password'        => Hash::make("Summer123!"),
+            "email_verified_at" => Carbon::now()
+        ]);
+
+        $role = CustomRole::findByName(Roles::MEMBER, 'api');
+        $role2 = CustomRole::findByName(Roles::ADMIN, 'api');
+        $this->saveUserRole($created, $role,  "Edmond");
+        $this->saveUserRole($created, $role2,  "Edmond");
 
     }
 
